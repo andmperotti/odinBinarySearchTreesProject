0201292f8a2314a9370782e47b0b76f3a87ffb69
Andrew Perotti <Perotti28@gmail.com>
2025-05-01 13:24:00 -0700

    Add logic for Tree constructor and buildTree
    Added two properties, first a property that takes in the argument and modifies it into something more usable, an array that is sorted and has unique values, second added a property that holds the return value of the buildTree method of the class which I made static so only the class itself can access it, which will be a binary search tree.

--- Tree.js
+++ Tree.js
@@ -2,7 +2,19 @@ import Node from "./Node.js";
 
 export default class {
   constructor(arr) {
-    this.arr = arr;
-    this.root;
+    this.arr = Array.from(new Set(arr)).sort((a, b) => (a < b ? -1 : 0));
+    this.root = this.#buildTree(this.arr, 0, this.arr.length - 1);
+  }
+  #buildTree(arr, start, end) {
+    if (start > end) {
+      return null;
+    }
+
+    let middle = Math.floor((start + end) / 2);
+    let root = new Node(arr[middle]);
+
+    root.left = this.#buildTree(arr, start, middle - 1);
+    root.right = this.#buildTree(arr, middle + 1, end);
+    return root;
   }
 }
