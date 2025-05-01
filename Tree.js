72efd096d05e49e4c8261ef2694c11be0a9dfa9d
Andrew Perotti <Perotti28@gmail.com>
2025-05-01 12:43:43 -0700

    Add starter code for Node and Tree files
    I've used a template repository for the simple fact that I had gemini create my eslint config file and it actually works now so instead of trying to figure that out at this time I'll reuse it here.
    The last project used linked tree and node classes but instead of reusing Node I instead rewrote it.

---
+++ Tree.js
@@ -0,0 +1,8 @@
+import Node from "./Node.js";
+
+export default class {
+  constructor(arr) {
+    this.arr = arr;
+    this.root;
+  }
+}
--- node_instance.js
+++
@@ -1,7 +0,0 @@
-export default class NodeInstance {
-  constructor(key, value, next = null) {
-    this.key = key;
-    this.value = value;
-    this.next = next;
-  }
-}
