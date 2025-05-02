b3e85d7dc7ef994dcd2ee5bffb2594e8df456bdd
Andrew Perotti <Perotti28@gmail.com>
2025-05-02 10:20:56 -0700

    Move prettyPrint into Tree class
    Removed the prettyPrint function expression, and moved it into the Tree class as a method. I did this because I thought it was ugly to have a function expression defined in the index.js where all the invocations are made.

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ import Tree from "./Tree.js";
 
 let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
-};
-
-console.log(prettyPrint(test.root));
+test.prettyPrint(test.root);
+test.insert(28);
+test.prettyPrint(test.root);
