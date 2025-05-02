d073da419d55a22cd1d6dfde8de4ca382bd090a9
Andrew Perotti <Perotti28@gmail.com>
2025-05-02 10:39:29 -0700

    Ad prettyPrint as a method
    Moved the prettyPrint method from index.js where it was a function expression into the Tree class as a method. I felt that it's easier to look at in the class constructor then mucking up the index.js file.

--- Tree.js
+++ Tree.js
@@ -5,6 +5,7 @@ export default class {
     this.arr = Array.from(new Set(arr)).sort((a, b) => (a < b ? -1 : 0));
     this.root = this.#buildTree(this.arr, 0, this.arr.length - 1);
   }
+
   #buildTree(arr, start, end) {
     if (start > end) {
       return null;
@@ -16,5 +17,23 @@ export default class {
     root.left = this.#buildTree(arr, start, middle - 1);
     root.right = this.#buildTree(arr, middle + 1, end);
     return root;
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        // eslint-disable-next-line prettier/prettier
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 }
