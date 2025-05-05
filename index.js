9a1538352b0d32a7f1186c909bdb5fa5d6b1820f
Andrew Perotti <Perotti28@gmail.com>
2025-05-05 13:20:28 -0700

    Add class method invocations
    Added method invocations as I needed to test newly added class methods.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import Tree from "./Tree.js";
 
 let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+// let test = new Tree([]);
 
 test.prettyPrint(test.root);
 test.insert(28);
+test.prettyPrint(test.root);
+console.log(test.root.right.left.right.right); //should be a node with value 28
+test.prettyPrint(test.root);
+test.delete(8);
 test.prettyPrint(test.root);
