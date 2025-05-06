3929d4182d69dba339df25e696227d187daa41de
Andrew Perotti <Perotti28@gmail.com>
2025-05-06 12:42:08 -0700

    Comment out newer invocations
    Commented out method invocations that trailed an invocation that lead to an unwanted result. The delete method I've noticed is not returning the desired output, and is deleting more than one node.

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import Tree from "./Tree.js";
 
 let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-// let test = new Tree([]);
 
 test.prettyPrint(test.root);
-test.insert(28);
+test.insert(28); //correct
 test.prettyPrint(test.root);
-console.log(test.root.right.left.right.right); //should be a node with value 28
+test.delete(23); //incorrect, deleting 23 as well
 test.prettyPrint(test.root);
-test.delete(8);
-test.prettyPrint(test.root);
-console.log(test.find(28));
-test.levelOrder((e) => console.log(e));
-test.levelOrder((e) => (e = e * e));
-test.prettyPrint(test.root);
+// console.log(test.find(28));
+// test.levelOrder((e) => console.log(e));
+// test.levelOrder((e) => (e = e * e));
+// test.prettyPrint(test.root);
